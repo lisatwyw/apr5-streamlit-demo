16111a8544a698b12102e77cea759e5acb21c390
lisatwyw <38703113+lisatwyw@users.noreply.github.com>
2024-03-21 21:25:48 +0000

    updated requirements

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,50 +1,39 @@
+
+import folium, geopy
 import streamlit as st
-import numpy as np
-import pandas as pd
-import altair as alt
+from branca.element import Figure
 
-# Page title
-st.set_page_config(page_title='Interactive Data Explorer', page_icon='📊')
-st.title('📊 Interactive Data Explorer')
+def map_loc( address ):
+  '''
+  Creates a map
+  '''
 
-with st.expander('About this app'):
-  st.markdown('**What can this app do?**')
-  st.info('This app shows the use of Pandas for data wrangling, Altair for chart creation and editable dataframe for data interaction.')
-  st.markdown('**How to use the app?**')
-  st.warning('To engage with the app, 1. Select genres of your interest in the drop-down selection box and then 2. Select the year duration from the slider widget. As a result, this should generate an updated editable DataFrame and line plot.')
+  geolocator = geopy.geocoders.Nominatim(user_agent="3")
+  location = geolocator.geocode( address )         
+  lx,ly=location.longitude, location.latitude
+
+  fig = Figure( width=400,height=200)
+  a_map = folium.Map(location = [lx,ly], zoom_start = 16)
   
-st.subheader('Which Movie Genre performs ($) best at the box office?')
+  p  = geopy.point.Point(lx, ly)
+  
+  gl = geopy.geocoders.Nominatim(user_agent="my_test") # Without the user_agent it raises a ConfigurationError.
+  site = gl.reverse(p)
+  site_name = site[0]
+  folium.Marker( location=[lx, ly], popup='Default popup Marker3',tooltip=site_name).add_to(m)
+  fig.add_child(a_map)
+  
+  return fig, a_map, site_name
 
-# Load data
-df = pd.read_csv('data/movies_genres_summary.csv')
-df.year = df.year.astype('int')
+try:
+  address = 'Cactus Club, Downtown'
+  fig, a_map, site_name = map_loc(address)
+except Exception as e:
+  print( e ) 
 
-# Input widgets
-## Genres selection
-genres_list = df.genre.unique()
-genres_selection = st.multiselect('Select genres', genres_list, ['Action', 'Adventure', 'Biography', 'Comedy', 'Drama', 'Horror'])
-
-## Year selection
-year_list = df.year.unique()
-year_selection = st.slider('Select year duration', 1986, 2006, (2000, 2016))
-year_selection_list = list(np.arange(year_selection[0], year_selection[1]+1))
-
-df_selection = df[df.genre.isin(genres_selection) & df['year'].isin(year_selection_list)]
-reshaped_df = df_selection.pivot_table(index='year', columns='genre', values='gross', aggfunc='sum', fill_value=0)
-reshaped_df = reshaped_df.sort_values(by='year', ascending=False)
-
-
-# Display DataFrame
-
-df_editor = st.data_editor(reshaped_df, height=212, use_container_width=True,
-                            column_config={"year": st.column_config.TextColumn("Year")},
-                            num_rows="dynamic")
-df_chart = pd.melt(df_editor.reset_index(), id_vars='year', var_name='genre', value_name='gross')
-
-# Display chart
-chart = alt.Chart(df_chart).mark_line().encode(
-            x=alt.X('year:N', title='Year'),
-            y=alt.Y('gross:Q', title='Gross earnings ($)'),
-            color='genre:N'
-            ).properties(height=320)
-st.altair_chart(chart, use_container_width=True)
+st.title( 'My first web app' )
+st.header( 'My favourite restaurant' )
+st.markdown("# Top heading")
+st.markdown("## Subheading")
+st.read_markdown_file( 'readme.md' )
+st.st_folium( a_map )
