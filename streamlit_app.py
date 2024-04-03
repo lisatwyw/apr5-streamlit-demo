f002f9b0360162495d61f92725aa717e2bb9262f
lisatwyw <38703113+lisatwyw@users.noreply.github.com>
2024-04-03 15:29:52 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,17 @@
+#    Copyright 2023 lisatwyw Lisa Y.W. Tang
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
 
 import folium, geopy
 import streamlit as st
@@ -7,8 +21,6 @@ from streamlit_folium import st_folium, folium_static
 import plotly.express as px
 import polars as pol
 import pandas as pd
-
-
 
 def map_loc( address ):
   '''
@@ -65,7 +77,8 @@ except:
     print( e ) 
   
 
-
+st.header( 'Show me data' )
+st.text( 'Below widget will try to generate graphs of an input CSV' )
 import pandas as pd
 file = st.file_uploader( 'Upload', type=['csv'] )
 try:      
